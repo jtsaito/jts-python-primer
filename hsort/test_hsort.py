f05fef21f93f8711ceedd0840fec07c075bb093f
jtsaito <jahn.saito@gmail.com>
2016-04-03 17:10:33 +0200

    Add docs and fix naming of some cases

--- hsort/test_hsort.py
+++ hsort/test_hsort.py
@@ -23,33 +23,39 @@ class TestHSort(unittest.TestCase):
         return random_list, sorted_list
 
     def test_sort_empty(self):
+        """Sort empty list"""
         self.assertEqual(self.hsort.sort([]), [])
 
     def test_sort_one(self):
+        """Sort list with one element"""
         self.assertEqual(self.hsort.sort([1]), [1])
 
     def test_sort_two_reversed(self):
+        """Sort reversed list with two elements"""
         self.assertEqual(self.hsort.sort([2, 1]), [1, 2])
 
     def test_sort_two_ordered(self):
+        """Sort sorted list with two elements"""
         self.assertEqual(self.hsort.sort([1, 2]), [1, 2])
 
-    def test_sort_three_ordered(self):
+    def test_sort_three_sorted(self):
+        """Sort sorted list with three elements"""
         self.assertEqual(self.hsort.sort([1, 2, 3]), [1, 2, 3])
 
     def test_sort_three_reversed(self):
+        """Sort reversed list with three elements"""
         self.assertEqual(self.hsort.sort([3, 2, 1]), [1, 2, 3])
 
     def test_sort_four_sorted(self):
+        """Sort sorted list with four elements"""
         self.assertEqual(self.hsort.sort([1,2,3,4]), [1,2,3,4])
 
     def test_sort_four_reverse(self):
-        self.assertEqual(self.hsort.sort([4,3,2,1]), [1,2,3,4])
-
-    def test_sort_four_random(self):
+        """Sort reversed list with four elements"""
         self.assertEqual(self.hsort.sort([4,3,2,1]), [1,2,3,4])
 
     def test_sort_n_random(self):
+        """sorts 16 random lists of size 33"""
         for random_seed in range(16):
             rand_list, sorted_list = _random_list(33, random_seed)
             self.assertEqual(self.hsort.sort(list(rand_list)), sorted_list)

27fa32e6901c4e54eab7580bfe61fddc04864abe
jtsaito <jahn.saito@gmail.com>
2016-04-03 16:30:48 +0200

    Rename variable becuase linter does not like it

--- hsort/hsort.py
+++ hsort/hsort.py
@@ -3,31 +3,31 @@ import functools
 class HSort:
     """Heap sort."""
 
-    def sort(self, list):
+    def sort(self, array):
         """Sort a list"""
-        self._build_heap(list)
+        self._build_heap(array)
 
-        length = len(list) - 1
-        return self._build_list(list, length)
+        length = len(array) - 1
+        return self._build_list(array, length)
 
-    def _build_heap(self, list):
-        return self._recursive_heapify(list, 0)
+    def _build_heap(self, array):
+        return self._recursive_heapify(array, 0)
 
-    def _recursive_heapify(self, list, index):
-        if index >= len(list): return list
+    def _recursive_heapify(self, array, index):
+        if index >= len(array): return array
 
-        self._sift_up(list, index)
+        self._sift_up(array, index)
 
-        return self._recursive_heapify(list, index + 1)
+        return self._recursive_heapify(array, index + 1)
 
-    def _sift_up(self, list, i):
-        if i <= 0: return list
+    def _sift_up(self, array, i):
+        if i <= 0: return array
 
         p = self._predecessor_index(i)
-        if list[p] < list[i]:
-            list[i], list[p] = list[p], list[i]
+        if array[p] < array[i]:
+            array[i], array[p] = array[p], array[i]
 
-        self._sift_up(list, i - 1)
+        self._sift_up(array, i - 1)
 
     def _predecessor_index(self, index):
         return 0 if index <= 0 else (index - 1) // 2
@@ -38,33 +38,33 @@ class HSort:
     def _right_successor_index(self, index):
         return (2 * index) + 2
 
-    def _build_list(self, list, index):
-        if index < 1: return list
+    def _build_list(self, array, index):
+        if index < 1: return array
 
-        if list[0] > list[index]:
-            list[0], list[index] = list[index], list[0]
-            self._sift_down(list, 0, index-1)
+        if array[0] > array[index]:
+            array[0], array[index] = array[index], array[0]
+            self._sift_down(array, 0, index-1)
 
-        return self._build_list(list, index-1)
+        return self._build_list(array, index-1)
 
-    def _sift_down(self, list, index, end_index):
-        if self._left_successor_index(index) >= end_index: return list
+    def _sift_down(self, array, index, end_index):
+        if self._left_successor_index(index) >= end_index: return array
 
-        max_index = self._max_index(list, index, end_index)
+        max_index = self._max_index(array, index, end_index)
 
         if max_index != index:
-            list[index], list[max_index] = list[max_index], list[index]
-            return self._sift_down(list, max_index, end_index)
+            array[index], array[max_index] = array[max_index], array[index]
+            return self._sift_down(array, max_index, end_index)
         else:
-            return list
+            return array
 
-    def _max_index(self, list, index, end_index):
+    def _max_index(self, array, index, end_index):
         left_index = self._left_successor_index(index)
         right_index = self._right_successor_index(index)
 
         indexes = [index, left_index]
         if right_index <= end_index: indexes.append(right_index)
 
-        def arg_max(a, b): return a if list[a] >= list[b] else b
+        def arg_max(a, b): return a if array[a] >= array[b] else b
 
         return functools.reduce(arg_max, indexes)
